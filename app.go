d049d253884cf535f458c4dd48e11ddd3c83b330
Daniel <dcmckay10@gmail.com>
2021-04-10 01:07:35 -0600

    Added ugly chained if-else for checking the id of the weather status and providing the proper ascii art. As of now, they all return the same cloud string

--- app.go
+++ app.go
@@ -55,14 +55,35 @@ func main() {
 }
 
 func printWeather(weatherData models.WeatherResponse) {
-	models.GetClouds()
+	processCondition(weatherData.Weather[0].Id)
 	fmt.Println()
 	fmt.Printf("\nCurrent weather in %s: \n", weatherData.Name)
 	fmt.Println()
 
 	color.Cyan.Printf("%.1f degrees F,", weatherData.Main.Temp)
 	color.FgLightRed.Printf(" feels like %.1f, ", weatherData.Main.Feels_like)
-	color.Magenta.Printf("%s \n", weatherData.Weather[0].Main)
+	color.Magenta.Printf("%s: %s \n", weatherData.Weather[0].Main, weatherData.Weather[0].Description)
 	fmt.Println()
+}
 
+func processCondition(id int) {
+	if id > 800 {
+			models.GetClouds()
+	} else if id == 800 {
+		models.GetClear()
+	} else if id >= 700 && id <= 741 {
+		models.GetFog()
+	} else if id > 741 && id < 781 {
+		models.GetAsh()
+	} else if id == 781 {
+		models.GetTornado()
+	} else if id >= 600 && id <= 700 {
+		models.GetSnow()
+	} else if id >= 500 && id < 600 {
+		models.GetRain()
+	} else if id >= 300 && id < 400 {
+		models.GetDrizzle()
+	} else {
+		models.GetThunderstorm()
+	}
 }
